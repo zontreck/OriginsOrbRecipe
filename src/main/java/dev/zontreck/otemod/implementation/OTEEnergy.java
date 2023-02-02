468df6d07e0fb394f354f03a55372379cb9cb19e
Aria <tarapiccari@gmail.com>
2023-02-01 19:50:32 -0700

    Add changed files

---
+++ src/main/java/dev/zontreck/otemod/implementation/OTEEnergy.java
@@ -0,0 +1,42 @@
+package dev.zontreck.otemod.implementation;
+
+import net.minecraftforge.energy.EnergyStorage;
+
+public abstract class OTEEnergy extends EnergyStorage
+{
+
+    public OTEEnergy(int capacity, int maxTransfer) {
+        super(capacity, maxTransfer);
+    }
+
+    @Override
+    public int extractEnergy(int max, boolean simulate)
+    {
+        int ex = super.extractEnergy(max, simulate);
+        if(ex != 0){
+            onChanged();
+        }
+
+        return ex;
+    }
+
+    @Override
+    public int receiveEnergy(int max, boolean simulate)
+    {
+        int rcv = super.receiveEnergy(max, simulate);
+        if(rcv!=0){
+            onChanged();
+        }
+
+        return rcv;
+    }
+
+    public int setEnergy(int energy){
+        this.energy=energy;
+        onChanged();
+        return energy;
+    }
+
+    public abstract void onChanged();
+    
+}
