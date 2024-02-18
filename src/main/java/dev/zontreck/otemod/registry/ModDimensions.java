77b9e1b22e831a51b0b0caae3953048cb7a6a3ef
Zontreck <tarapiccari@gmail.com>
2024-02-18 07:47:02 -0700

    Add threshold to the mod dimensions file

--- src/main/java/dev/zontreck/otemod/registry/ModDimensions.java
+++ src/main/java/dev/zontreck/otemod/registry/ModDimensions.java
@@ -6,8 +6,13 @@ import net.minecraft.resources.ResourceLocation;
 public class ModDimensions
 {
     public static final ResourceLocation BUILDER = new ResourceLocation(OTEMod.MOD_ID, "builder");
+    public static final ResourceLocation THRESHOLD = new ResourceLocation(OTEMod.MOD_ID, "threshold");
     public static String BUILDER_DIM()
     {
         return BUILDER.getNamespace() + ":" + BUILDER.getPath();
+    }
+    public static String THRESHOLD_DIM()
+    {
+        return THRESHOLD.getNamespace() + ":" + THRESHOLD.getPath();
     }
 }
